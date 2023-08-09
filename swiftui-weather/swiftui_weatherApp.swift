6f4e0eb33d6cda49de4144aa6d07300571ff2c63
Ganesh Kumar <ganeshkumar.s.developer@gmail.com>
2023-08-09 08:30:17 +0530

    Initial Commit

---
+++ swiftui-weather/swiftui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weatherApp.swift
+//  swiftui-weather
+//
+//  Created by Ganesh Kumar on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
